7fcf53bd5e2dd01667bd04c719f33a310c9cafd1
Mauricio Rojas <mrojasb2000@gmail.com>
2019-12-16 10:49:14 -0300

    Add example project.

---
+++ chapter01/templates/example/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/mrojasb2000/go-programming-cookbook/chapter01/templates"
+
+func main() {
+	if err := templates.RunTemplate(); err != nil {
+		panic(err)
+	}
+
+	if err := templates.InitTemplates(); err != nil {
+		panic(err)
+	}
+
+	if err := templates.HTMLDifferences(); err != nil {
+		panic(err)
+	}
+}
