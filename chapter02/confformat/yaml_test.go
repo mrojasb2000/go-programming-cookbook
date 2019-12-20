3da2508afd61bf367035f85ab3e7e3dddf8b39f9
Mauricio Rojas <mrojasb2000@gmail.com>
2019-12-20 16:27:33 -0300

    Add Decode unit test.

--- chapter02/confformat/yaml_test.go
+++ chapter02/confformat/yaml_test.go
@@ -37,3 +37,33 @@ func TestYAMLData_ToYAML(t *testing.T) {
 		})
 	}
 }
+
+func TestYAMLData_Decode(t *testing.T) {
+	type fields struct {
+		Name string
+		Age int
+	}
+	type args struct {
+		data []byte
+	}
+	tests := []struct {
+		name string
+		fields fields
+		args args
+		wantErr bool
+	}{
+		{"base-case", fields{}, args{[]byte("name: Someone\nage: 100\n")}, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T){
+			td := &YAMLData{
+				Name: tt.fields.Name,
+				Age: tt.fields.Age,
+			}
+			if err := td.Decode(tt.args.data); (err != nil) != tt.wantErr {
+				t.Errorf("YAMLData.Decode() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
