aefc16f721faceb8f087a6369112b8e714fd065a
Mauricio Rojas <mrojasb2000@gmail.com>
2019-12-20 11:41:04 -0300

    Add main file to project.

---
+++ chapter02/confformat/example/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/mrojasb2000/go-programming-cookbook/chapter02/confformat"
+
+func main() {
+	if err := confformat.MarshalAll(); err != nil {
+		panic(err)
+	}
+
+	if err := confformat.UnmarshalAll(); err != nil {
+		panic(err)
+	}
+
+	if err := confformat.OtherJSONExamples(); err != nil {
+		panic(err)
+	}
+}
