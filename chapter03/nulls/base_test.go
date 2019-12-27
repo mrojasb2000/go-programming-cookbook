4efe07f0ec978173957a8ad5d289c40bd241af60
Mauricio Rojas <mrojasb2000@gmail.com>
2019-12-27 12:23:46 -0300

    Add BaseEncoding unit test.

---
+++ chapter03/nulls/base_test.go
@@ -0,0 +1,20 @@
+package nulls
+
+import "testing"
+
+func TestBaseEncoding(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{"base-case", false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := BaseEncoding(); err != nil {
+				t.Errorf("BaseEnconding() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
