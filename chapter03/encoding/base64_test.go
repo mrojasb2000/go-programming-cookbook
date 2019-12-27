3e773e7a48414632f6c077b0c75597dcbc272723
Mauricio Rojas <mrojasb2000@gmail.com>
2019-12-27 15:38:10 -0300

    Add Base64ExampleEncoder unit test.

--- chapter03/encoding/base64_test.go
+++ chapter03/encoding/base64_test.go
@@ -18,3 +18,20 @@ func TestBase64Example(t *testing.T) {
 		})
 	}
 }
+
+func TestBase64ExampleEncoder(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{"base-case", false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Base64ExampleEncoder(); (err != nil) != tt.wantErr {
+				t.Errorf("Base64ExampleEncoder() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
