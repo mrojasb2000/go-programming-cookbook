f112c8d4a27e93f17737a55e0951487a03762af6
Mauricio Rojas <mrojasb2000@gmail.com>
2019-12-27 11:22:27 -0300

    Add basic operations with nil/omitted values on json.

---
+++ chapter03/nulls/example/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mrojasb2000/go-programming-cookbook/chapter03/nulls"
+)
+
+func main() {
+
+	if err := nulls.BaseEncoding(); err != nil {
+		panic(err)
+	}
+	fmt.Println()
+
+	if err := nulls.PointerEncoding(); err != nil {
+		panic(err)
+	}
+	fmt.Println()
+
+	if err := nulls.NullEncoding(); err != nil {
+		panic(err)
+	}
+}
