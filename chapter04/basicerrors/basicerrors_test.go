f32191d5c8bb24e6cadd48f4d21481a9a8690e44
Mauricio Rojas <mrojasb2000@gmail.com>
2019-12-30 16:14:58 -0300

    Add basic errors unit test.

---
+++ chapter04/basicerrors/basicerrors_test.go
@@ -0,0 +1,17 @@
+package basicerrors
+
+import "testing"
+
+func TestBasicErrors(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{"base-case"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			BasicErrors()
+		})
+	}
+}
