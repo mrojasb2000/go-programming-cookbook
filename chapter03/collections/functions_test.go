8a08abc22265dff672853f38f978a8ee527e9f77
Mauricio Rojas <mrojasb2000@gmail.com>
2019-12-30 14:16:18 -0300

    Add increment version unit test.

--- chapter03/collections/functions_test.go
+++ chapter03/collections/functions_test.go
@@ -25,3 +25,24 @@ func TestLowerCaseData(t *testing.T) {
 		})
 	}
 }
+
+func TestIncrementVersion(t *testing.T) {
+	type args struct {
+		w WorkWith
+	}
+	tests := []struct {
+		name string
+		args args
+		want WorkWith
+	}{
+		{"base-case", args{WorkWith{"Test", 0}}, WorkWith{"Test", 1}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IncrementVersion(tt.args.w); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("IncrementVersion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
