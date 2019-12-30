e54d5a0ee11006856928d8a1169036710eb246fa
Mauricio Rojas <mrojasb2000@gmail.com>
2019-12-30 11:21:08 -0300

    Add serialization unit test.

---
+++ chapter03/tags/serialize_test.go
@@ -0,0 +1,31 @@
+package tags
+
+import "testing"
+
+func TestSerializeStructStrings(t *testing.T) {
+	type args struct {
+		s interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{"base-case", args{Person{Name: "test"}}, "name:test;city:;State:;", false},
+		{"base-case", args{Person{Name: "name", City: "city", State: "state", Misc: "misc", Year: 2017}}, "name:name;city:city;State:state;", false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := SerializeStructStrings(tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SerializeStructStrings() error = %v, want %v", err, tt.want)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("SerializeStructStrings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
