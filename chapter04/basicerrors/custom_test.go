1f3db3516e53a59450ec477703601f588911f21b
Mauricio Rojas <mrojasb2000@gmail.com>
2019-12-30 16:31:22 -0300

    Add custom error unit test

--- chapter04/basicerrors/custom_test.go
+++ chapter04/basicerrors/custom_test.go
@@ -19,3 +19,21 @@ func TestCustomError_Error(t *testing.T) {
 		})
 	}
 }
+
+func TestSomeFunc(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{"base-case", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := SomeFunc(); (err != nil) != tt.wantErr {
+				t.Errorf("SomeFunc() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+
+}
