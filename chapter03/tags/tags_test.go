b580ab1db58a2a5a3ea764fa113cc284ac2ff94f
Mauricio Rojas <mrojasb2000@gmail.com>
2019-12-30 11:47:03 -0300

    Add full struct unit test.

--- chapter03/tags/tags_test.go
+++ chapter03/tags/tags_test.go
@@ -18,3 +18,20 @@ func TestEmptyStruct(t *testing.T) {
 		})
 	}
 }
+
+func TestFullStruct(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{"base-case", false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := FullStruct(); (err != nil) != tt.wantErr {
+				t.Errorf("FullStruct() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
