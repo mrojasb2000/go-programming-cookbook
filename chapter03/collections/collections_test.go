740e2faa837032e20c88e544aab7537366ae3eef
Mauricio Rojas <mrojasb2000@gmail.com>
2019-12-30 13:44:31 -0300

    Add Map function unit test.

--- chapter03/collections/collections_test.go
+++ chapter03/collections/collections_test.go
@@ -29,3 +29,26 @@ func TestFilter(t *testing.T) {
 		})
 	}
 }
+
+func TestMap(t *testing.T) {
+	ws := []WorkWith{WorkWith{"test", 1}}
+	type args struct {
+		ws []WorkWith
+		f  func(w WorkWith) WorkWith
+	}
+	tests := []struct {
+		name string
+		args args
+		want []WorkWith
+	}{
+		{"base-case", args{ws, func(w WorkWith) WorkWith { return w }}, []WorkWith{WorkWith{"test", 1}}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Map(tt.args.ws, tt.args.f); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Map() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
