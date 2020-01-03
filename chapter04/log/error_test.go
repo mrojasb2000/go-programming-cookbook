c51e8a3e71e7e525e897d42d2ac6d4c92729f5f7
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-03 09:14:38 -0300

    Add through error unit test.

--- chapter04/log/error_test.go
+++ chapter04/log/error_test.go
@@ -18,3 +18,20 @@ func TestOrifinalError(t *testing.T) {
 		})
 	}
 }
+
+func TestThroughError(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{"base-case", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := PassThroughError(); (err != nil) != tt.wantErr {
+				t.Errorf("PassThroughError() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
