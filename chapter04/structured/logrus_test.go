3b38d30f1a65ed4f077db1880f2da89edebf8e7b
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-03 10:20:59 -0300

    Add logrus unit test.

---
+++ chapter04/structured/logrus_test.go
@@ -0,0 +1,21 @@
+package structured
+
+import (
+	"testing"
+
+	//"github.com/sirupsen/logrus"
+)
+
+func TestLogrus(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{"base-case"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Logrus()
+		})
+	}
+}
