5e9791cc000f2a73a2bea1aa29eddf0cec83ac23
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-03 09:12:19 -0300

    Add orginal error unit test.

---
+++ chapter04/log/error_test.go
@@ -0,0 +1,20 @@
+package log
+
+import "testing"
+
+func TestOrifinalError(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{"base-case", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := OriginalError(); (err != nil) != tt.wantErr {
+				t.Errorf("OriginalError() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
