b905bb9d398b6cd4cf4f442fc693aa8e2595a5e9
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-06 14:28:45 -0300

    Adds debug unit test.

--- chapter04/global/global_test.go
+++ chapter04/global/global_test.go
@@ -63,3 +63,21 @@ func TestWithField(t *testing.T) {
 		})
 	}
 }
+
+func TestDebug(t *testing.T) {
+	type args struct {
+		args []interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"base-case", args{}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Debug(tt.args.args...)
+		})
+	}
+}
