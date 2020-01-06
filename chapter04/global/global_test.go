ce34b358cd080570b21d0e6cff7e3cddf2f836d4
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-06 13:10:59 -0300

    Adds set logger instance unit test.

--- chapter04/global/global_test.go
+++ chapter04/global/global_test.go
@@ -1,6 +1,10 @@
 package global
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
 
 func TestInit(t *testing.T) {
 	tests := []struct {
@@ -15,6 +19,24 @@ func TestInit(t *testing.T) {
 			if err := Init(); (err != nil) != tt.wantErr {
 				t.Errorf("Init() error = %v, wantErr %v", err, tt.wantErr)
 			}
+		})
+	}
+}
+
+func TestSetLog(t *testing.T) {
+	type args struct {
+		l *logrus.Logger
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"base-case", args{logrus.New()}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			SetLog(tt.args.l)
 		})
 	}
 }
