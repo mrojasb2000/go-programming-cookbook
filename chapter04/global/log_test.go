ef45d74f4c47308d7de4336a66ec611bab2827dc
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-06 14:38:23 -0300

    Adds use log unit test.

---
+++ chapter04/global/log_test.go
@@ -0,0 +1,20 @@
+package global
+
+import "testing"
+
+func TestUseLog(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{"base-case", false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := UseLog(); (err != nil) != tt.wantErr {
+				t.Errorf("UseLog() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
