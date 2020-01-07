eeb0f3b433b11d0dc7be7ec596541f814be524c3
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-07 15:23:47 -0300

    Adds broadcast function.

---
+++ chapter05/udp/server/broadcast.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"sync"
+	"time"
+)
+
+type connections struct {
+	addrs map[string]*net.UDPAddr
+	// lock for modifying the map
+	mu sync.Mutex
+}
+
+func broadcast(conn *net.UDPConn, conns *connections) {
+	count := 0
+	for {
+		count++
+		conns.mu.Lock()
+		// loop over known addresses
+		for _, retAddr := range conns.addrs {
+			// send a message to them all
+			msg := fmt.Sprintf("sent %d", count)
+			if _, err := conn.WriteToUDP([]byte(msg), retAddr); err != nil {
+				fmt.Printf("error encountered: %s\n", err)
+				continue
+			}
+		}
+		conns.mu.Unlock()
+		time.Sleep(1 * time.Second)
+	}
+}
