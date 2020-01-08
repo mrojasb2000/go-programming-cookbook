90fa2d465d6a9ecc4617166249809b7f9a927e8f
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-08 15:29:06 -0300

    Adds lookup show fields info.

--- chapter05/dns/dns_test.go
+++ chapter05/dns/dns_test.go
@@ -24,3 +24,30 @@ func TestLookupAddress(t *testing.T) {
 		})
 	}
 }
+
+func TestLooup_String(t *testing.T) {
+	type fields struct {
+		cname string
+		hosts []string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{"empty", fields{"", []string{}}, ""},
+		{"not empty", fields{"cname", []string{"golang.org"}}, "cname IN A golang.org\n"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &Lookup{
+				cname: tt.fields.cname,
+				hosts: tt.fields.hosts,
+			}
+			if got := d.String(); got != tt.want {
+				t.Errorf("Lookup.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
